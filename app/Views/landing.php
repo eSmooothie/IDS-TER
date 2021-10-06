a51cfa6b532e4d2cb37c353cce7985ba655fc596
Sherwin Sandoval <33995402+eSmooothie@users.noreply.github.com>
2021-10-06 19:16:02 +0800

    init

---
+++ app/Views/landing.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="<?php echo base_url(); ?>/bootstrap-5.0.2/css/bootstrap.min.css">
+  <script src="<?php echo base_url(); ?>/bootstrap-5.0.2/js/bootstrap.min.js" charset="utf-8"></script>
+  <title>IDS | TER</title>
+</head>
+<body>
+<div class="" style="">
+
+</div>
+</body>
+</html>
