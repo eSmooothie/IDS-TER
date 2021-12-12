0bee6d7a5c612041636002a7aebb2d6d28ce7b2f
Sherwin Sandoval <33995402+eSmooothie@users.noreply.github.com>
2021-12-12 16:17:42 +0800

    Separate adding student page

---
+++ app/Views/admin/pages/addStudent.php
@@ -0,0 +1,24 @@
+<!-- content -->
+<div class="col ps-3" id="studentsColContainer">
+  <div class="border rounded p-3 bg-light bg-gradient d-flex align-items-center justify-content-between mb-4">
+    <div class="">
+      <a href="<?php echo "$baseUrl/admin/student"; ?>" class="btn btn-primary">
+        <i class="fas fa-arrow-left"></i> Back</a>
+    </div>
+  </div>
+  <!-- Bulk -->
+  <div class="border rounded p-3 bg-light bg-gradient mb-3">
+    <p>Add BULK</p>
+    <form class="" id="bulk" method="post">
+
+    </form>
+  </div>
+
+  <!-- Individual -->
+  <div class="border rounded p-3 bg-light bg-gradient mb-3">
+    <p>Add Individual</p>
+    <form class="" id="individual" method="post">
+
+    </form>
+  </div>
+</div>
